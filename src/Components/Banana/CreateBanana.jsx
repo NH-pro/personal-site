0020d13d3d5a2a794dbd433c4a64c087ee8785aa
Neil Hanson <pro.neilhanson@gmail.com>
2022-09-06 19:00:06 -0500

    fixed a bug where bananas would be incorrectly deleted

--- src/Components/Banana/CreateBanana.jsx
+++ src/Components/Banana/CreateBanana.jsx
@@ -1,13 +1,15 @@
-function bananaClickHandle() {
-    document.getElementById(`bananaId`).remove();
-}
+let bananaIdNumCount = 0;
 
 function CreateBanana() {
+    bananaIdNumCount ++;
+
+    const bananaId = bananaIdNumCount;
+
     // This defines a new creation of the banana element
     const newBananaImage = document.createElement("img");
 
     // Adds attributes to the banana element
-    newBananaImage.setAttributeNS(null, 'id', `bananaId`);
+    newBananaImage.setAttributeNS(null, 'id', `bananaId${bananaId}`);
     newBananaImage.setAttributeNS(null, 'class', 'banana');
     newBananaImage.setAttributeNS(null, 'alt', 'bananaPic');
     newBananaImage.setAttributeNS(null, 'src', '../images/banana.png');
@@ -20,8 +22,17 @@ function CreateBanana() {
     
     // Delete old bananas
     setTimeout(() => {
-        document.getElementById(`bananaId`).remove();
+        if(document.getElementById(`bananaId${bananaId}`)) {
+            document.getElementById(`bananaId${bananaId}`).remove();
+            console.log('Banana timed out, so it was removed.')
+        }
     }, 4000)
+
+    // Delete clicked bananas and add them to the scoreboard
+    function bananaClickHandle() {
+        document.getElementById(`bananaId${bananaId}`).remove();
+        console.log('You got the banana!')
+    }
 }
 
 export default CreateBanana;
